2cbb1d089c3ebd345ddef5efbd5988e0993575f2
Da2dalus <63458929+Da2dalus@users.noreply.github.com>
2021-05-18 11:53:50 +0200

    Update b0b.py

--- b0b.py
+++ b0b.py
@@ -13,14 +13,19 @@ client = discord.Client()
 
 bad_words = ["fucker", "stupid", "fucking", "asshole", "shit", "fuck", "dumb", "ass", "nigger", "dick", "but"]
 
-class Slapper(commands.Converter):
-    async def convert(self, ctx, argument):
-        to_slap = random.choice(ctx.guild.members)
-        return '{0.author} slapped {1} because *{2}*'.format(ctx, to_slap, argument)
-
 @client.event
 async def on_ready():
     print("Started bot as {0.user}".format(client))
+    
+@client.event
+async def on_member_join(member):
+    print("Recognised that a member called " + member.name + " joined")
+    await client.send_message("Welcome to this server " + member.name)
+
+@client.event
+async def on_member_leave(member):
+    print("Recognised that a member called " + member.name + " left")
+    await client.send_message(member.name + " left the server. Fuck you!!!!")
 
 @client.event
 async def on_message(message):
@@ -28,33 +33,32 @@ async def on_message(message):
     if message.author == client.user:
         return
     
-    elif any (word in message for word in bad_words):
+    elif any (word in message.content for word in bad_words):
         await message.channel.send("You motherfucker! Stop fucking around with those bad words asshole!!!!!")
+    
+    elif message.content == "!duck.info":
+        await message.channel.send("Hello! I am B0B. I am a bot that hates violence and swearing. \n Use '!duck.help' to list all my commands.")
+    
+    elif message.content == "!duck.help":
+        await message.channel.send("List of all B0B commands: \n duck = If you want to duck someone. \n slap = Slap someone. \n fight = start a food fight \n challenge = challenge someone to a duel.")
+        
+    elif message.content.startswith("!duck.duck"):
+        name = message.content.split("!duck.duck ",1)[1] 
+        await message.channel.send("Fuck you " + name + "!!!!!")
 
-@client.event
-async def on_member_join(member):
-    print("Recognised that a member called " + member.name + " joined")
-    await client.send_message("Welcome to this server " + member)
+    elif message.content.startswith("!duck.slap"):
+        name = message.content.split("!duck.slap ",1)[1] 
+        await message.channel.send(message.author + " slapped " + name + "!!!!! That was brutal!!!")
+   
+    elif message.content == "!duck.fight":
+        await message.channel.send(message.author + " started a foot fight in the cafeteria!!! Watch out for that taco....")
 
-@client.event
-async def on_member_leave(member):
-    print("Recognised that a member called " + member.name + " left")
-    await client.send_message(member + " left the server. Fuck you!!!!")
-
-@bot.command()
-async def info(ctx):
-    await ctx.send("Hello! I am B0B. I am a bot that hates violence and swearing. \n Use '!duck.help' to list all my commands.")
-
-@bot.command()
-async def help(ctx):
-    await ctx.send("List of all B0B commands: \n !duck.duck = If you want to duck someone. \n !duck.slap = Slap someone.")
-
-@bot.command()
-async def duck(ctx, name):
-    await ctx.send("Fuck " + name + "!!!!!")
-
-@bot.command()
-async def slap(ctx, *, reason: Slapper):
-    await ctx.send(reason)
+    elif message.content.startswith("!duck.challenge"):
+        name = message.content.split("!duck.challenge ",1)[1] 
+        await message.channel.send(message.author + " challenged " + name + " to a duel! \n Do you accept " + name + "? Or are you going to run away like a coward!!)
+   
+    elif message.content.startswith("!duck"):
+        arg = message.content.split("!duck.",1)[1] 
+        await message.channel.send(message.author + " " + arg + " is not an available option...")
 
 client.run(token)
